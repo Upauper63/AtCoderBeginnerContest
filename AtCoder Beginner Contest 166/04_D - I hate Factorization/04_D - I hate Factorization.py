8db715624c1a5c4520218675fb196c72f0928dfc
Upauper63 <syunufkin@gmail.com>
2021-01-29 20:50:46 +0900

    166-D

---
+++ AtCoder Beginner Contest 166/04_D - I hate Factorization/04_D - I hate Factorization.py
@@ -0,0 +1,11 @@
+import os
+import sys
+f = open('04_D - I hate Factorization.txt', 'r')
+sys.stdin = f
+
+X = int(input())
+for i in range(-1000, 1000):
+    for j in range(-1000, 1000):
+        if i ** 5 - j ** 5 == X:
+            print(i, j)
+            exit()
