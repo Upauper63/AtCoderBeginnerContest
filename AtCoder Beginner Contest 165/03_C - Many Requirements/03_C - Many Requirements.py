f3d214361bcf4e128c359974971ca187862eeff0
Upauper63 <syunufkin@gmail.com>
2021-02-01 22:06:41 +0900

    165-C

---
+++ AtCoder Beginner Contest 165/03_C - Many Requirements/03_C - Many Requirements.py
@@ -0,0 +1,24 @@
+import os
+import sys
+f = open('03_C - Many Requirements.txt', 'r')
+sys.stdin = f
+
+import os
+import sys
+f = open('03_C - Many Requirements.txt', 'r')
+sys.stdin = f
+
+import itertools
+N, M, Q = map(int, input().split(' '))
+ls = []
+for i in range(Q):
+    ls.append(list(map(int, input().split(' '))))
+combis = itertools.combinations_with_replacement(range(1, M + 1), N)
+rst = 0
+for combi in combis:
+    tmp = 0
+    for a, b, c, d in ls:
+        if combi[b - 1] - combi[a - 1] == c:
+            tmp += d
+    rst = max(rst, tmp)
+print(rst)
