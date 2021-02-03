a7c8f7f4c27428fa7d5da7e4ed8fe9035626f380
Upauper63 <syunufkin@gmail.com>
2021-02-04 02:07:17 +0900

    163-D

---
+++ AtCoder Beginner Contest 163/04_D - Sum of Large Numbers/04_D - Sum of Large Numbers.py
@@ -0,0 +1,11 @@
+import os
+import sys
+f = open('04_D - Sum of Large Numbers.txt', 'r')
+sys.stdin = f
+
+N, K = map(int, input().split(' '))
+rst = 0
+for i in range(K, N + 2):
+    rst += (N - i + 1) * i + 1
+    rst %= 10 ** 9 + 7
+print(rst)
