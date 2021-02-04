b091b28f4e92a216947c516b625b73affc1321fe
Upauper63 <syunufkin@gmail.com>
2021-02-04 17:43:20 +0900

    162-D

---
+++ AtCoder Beginner Contest 162/04_D - RGB Triplets/04_D - RGB Triplets.py
@@ -0,0 +1,24 @@
+import os
+import sys
+f = open('04_D - RGB Triplets.txt', 'r')
+sys.stdin = f
+
+import collections
+N = int(input())
+S = list(input())
+cnt_ls = collections.Counter(S)
+rst = 1
+if len(cnt_ls) < 3:
+    print(0)
+    exit()
+for i in cnt_ls.values():
+    rst *= i
+for d in range(1, N):
+    for i in range(N):
+        j = i + d
+        k = j + d
+        if k >= N:
+            break
+        if S[i] != S[j] and S[i] != S[k] and S[j] != S[k]:
+            rst -= 1
+print(rst)
