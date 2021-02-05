979a617873aaafbfad7980b38e1a1cce8ae2051e
Upauper63 <syunufkin@gmail.com>
2021-02-05 15:51:25 +0900

    161-D

---
+++ AtCoder Beginner Contest 161/04_D - Lunlun Number/04_D - Lunlun Number.py
@@ -0,0 +1,23 @@
+import os
+import sys
+f = open('04_D - Lunlun Number.txt', 'r')
+sys.stdin = f
+
+K = int(input())
+import os
+import sys
+f = open('04_D - Lunlun Number.txt', 'r')
+sys.stdin = f
+
+K = int(input())
+lunlun = [i for i in range(1, 10)]
+p = 0
+while len(lunlun) < K:
+    last = lunlun[p] % 10
+    if last - 1 >= 0:
+        lunlun.append(lunlun[p] * 10 + last - 1)
+    lunlun.append(lunlun[p] * 10 + last)
+    if last + 1 <= 9:
+        lunlun.append(lunlun[p] * 10 + last + 1)
+    p += 1
+print(lunlun[K - 1])
