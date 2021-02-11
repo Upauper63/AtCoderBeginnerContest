b84383b7e8bf269774710b278ce6c7a267062f00
Upauper63 <syunufkin@gmail.com>
2021-02-12 02:32:18 +0900

    159-D

---
+++ AtCoder Beginner Contest 159/04_D - Banned K/04_D - Banned K.py
@@ -0,0 +1,18 @@
+import os
+import sys
+f = open('04_D - Banned K.txt', 'r')
+sys.stdin = f
+
+import collections
+N = int(input())
+ls = list(map(int, input().split(' ')))
+cnt_dic = collections.Counter(ls)
+cnt_ls = [0 for i in range(N + 1)]
+for a, b in cnt_dic.items():
+    cnt_ls[a] = (b * (b - 1) // 2)
+sum_val = sum(cnt_ls)
+for i in ls:
+    if cnt_ls[i]:
+        print(sum_val - (cnt_dic[i] - 1))
+    else:
+        print(sum_val)
