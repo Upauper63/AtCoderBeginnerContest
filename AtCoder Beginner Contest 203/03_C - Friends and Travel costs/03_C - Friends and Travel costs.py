36d7a5c686631728e0d8595fcc802e4757291158
Upauper63 <syunufkin@gmail.com>
2021-06-08 11:57:26 +0900

    203-C fix

--- AtCoder Beginner Contest 203/03_C - Friends and Travel costs/03_C - Friends and Travel costs.py
+++ AtCoder Beginner Contest 203/03_C - Friends and Travel costs/03_C - Friends and Travel costs.py
@@ -5,25 +5,17 @@ sys.stdin = f
 import collections
 n, k = map(int, input().split(' '))
 dic = collections.defaultdict(int)
-for i in range(n):
+for _ in range(n):
     a, b = map(int, input().split(' '))
     dic[a] += b
 dic = sorted(dic.items())
-cumsum_dic = {}
-val = 0
-for t in dic:
-    val += t[1]
-    cumsum_dic[t[0]] = val
-max_val = 0
-for s, t in cumsum_dic.items():
-    if s - 1 >= max_val + k:
-        print(k + max_val)
+val = k
+for s, t in dic:
+    if s > val:
+        print(val)
         exit()
-    if k + t < s:
-        print(k + t)
-        exit()
-    max_val = t
-if 10 ** 100 <= k + max_val:
+    val += t
+if val >= 10 ** 100:
     print(10 ** 100)
 else:
-    print(k + max_val)
+    print(val)
